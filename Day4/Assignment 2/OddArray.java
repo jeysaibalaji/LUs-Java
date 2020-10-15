6a37e3243c6660a785b8ed111759518fdfc02227
Jey Sai Balaji <61592154+jeysaibalaji@users.noreply.github.com>
2020-10-14 21:37:35 -0700

    Add files via upload

---
+++ Day4/Assignment 2/OddArray.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // datatype[] arr= new datatype[size]
+        int[] Numbers=new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+         for(int i =0;i<Numbers.length;i=i+2 ){
+             System.out.println(Numbers[i]);
+         }
+
+    }
+}
